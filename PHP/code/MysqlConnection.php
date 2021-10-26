87ae652b76239354973cb06254787529eb87dbfe
affandi2204 <m.affandi2016@gmail.com>
2021-10-26 21:59:54 +0700

    change mysql connection

--- PHP/code/MysqlConnection.php
+++ PHP/code/MysqlConnection.php
@@ -3,9 +3,9 @@
     $database = "tutor_query"; // db name tutor_query or other db name you have or created
     $username = "root"; // db username default root 
     $password = ""; // db password default null
-    $connect = mysqli_connect($hostname, $username, $password, $database);
+    $connect = new mysqli($hostname, $username, $password, $database);
     // Checking connection   
     if (!$connect) {
-        die("Database does not connected: " . mysqli_connect_error());
+        die("Database does not connected: " . $connect->connect_error);
     }
 ?>
